6ed227f62dae7c5607e102b280c0912e4bf6ad58
Arturo <grge.dev@gmail.com>
2020-08-07 14:02:43 -0600

    navegacion funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,61 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//components
+import { SigninComponent } from "./components/signin/signin.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { FeaturesComponent } from "./components/features/features.component";
+import { HomeComponent } from "./components/home/home.component";
+import { DownloadComponent } from "./components/download/download.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { TeamComponent } from "./components/team/team.component";
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  
+  {
+    path: 'features',
+    component: FeaturesComponent
+  },
+  
+  {
+    path: 'download',
+    component: DownloadComponent
+  },
+  
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { DownloadComponent } from './components/download/download.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TeamComponent } from './components/team/team.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FeaturesComponent,
+    DownloadComponent,
+    ContactComponent,
+    TeamComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
